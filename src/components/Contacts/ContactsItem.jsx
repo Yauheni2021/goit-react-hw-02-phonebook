6917bcf86fdb9a8584a46003188d48e762b769c0
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-07-16 15:28:36 +0200

    add phonebooks components

--- src/components/Contacts/ContactsItem.jsx
+++ src/components/Contacts/ContactsItem.jsx
@@ -1,5 +1,11 @@
-
+import {ContactItem, ContactBtn} from './Contacts.styled'
 
 export const ContactsListItem = ({ id, name, number, onClick }) => {
-
-}
+    return (
+        <ContactItem>
+            <p>{name}:</p>
+            <p>{number}:</p>
+            <ContactBtn id={id} onClick={() => onClick(id)}>Remove</ContactBtn>
+        </ContactItem>
+    );
+};
